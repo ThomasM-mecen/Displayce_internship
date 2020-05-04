12855753ca6a5910f2d895be9b39e37f09be496e
hayssam <hayssam.soueidan@displayce.com>
2020-05-04 17:13:34 +0200

    minor(tools): optimised setup script

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,9 +4,12 @@ ROOT_DIR=${CURRENT_DIR}/..
 VENV_DIR=${ROOT_DIR}/venv
 
 # create virtualenv
-virtualenv --python=python3.7 ${VENV_DIR}
+if [ ! -d ${VENV_DIR} ]
+then
+	virtualenv --python=python3.7 ${VENV_DIR}
+fi
 # install requirements
 
-source ${VENV_DIR}/bin/activate && pip3.6 install -r ${ROOT_DIR}/requirements.txt
+source ${VENV_DIR}/bin/activate && ${VENV_DIR}/bin/pip install -r ${ROOT_DIR}/requirements.txt
 
 
